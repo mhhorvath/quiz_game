3782836692bca2b61ef00602ded713617089bdd1
whitakerin <ewhitaker275@gmail.com>
2025-10-26 10:52:38 +0000

    updates to questions

--- src/app/questions.ts
+++ src/app/questions.ts
@@ -37,7 +37,7 @@ export const questions: Array<Question> = [
   },
   {
     id: 3,
-    question: "How do you like your whiskey",
+    question: "How do you like your whiskey?",
     options: [
       { text: "straight", vec: [-2, 8, 1, 2, 9, -6, 4, 3, -5, 7] },
       { text: "cheap", vec: [0, -6, 6, -3, -2, -6, 3, 1, 1, 8]},
@@ -45,44 +45,15 @@ export const questions: Array<Question> = [
       { text: "not", vec: [7, 6, -9, 5, -3, 7, 6, 9, 2, 0] },
     ],
   },
-  {
-    id: 4,
-    question: "How close are you to retirement?",
-    options: [
-      { text: "This is my retirement party", vec: [-7, 2, 8, 7, -5, 5, 7, 6, -7, 4]},
-      { text: "Years....", vec: [0, -3, 9, 4, 5, -9, 0, 2, -9, 6] },
-      { text: "Hundreds of thousands", vec: [6, 2, 6, -7, 0, 6, -8, 0, 7, 9] },
-      { text: "I won't be able to retire", vec: [6, 6, 3, -0, 5, 0, 5, -4, -4, 3] },
-    ],
-  },
+  
   {
     id: 5,
     question: "Musical theatre?",
     options: [
       { text: "Slay the house boots down", vec: [9, -3, 8, 0, 0, 5, -8, 6, -1, 0]},
       { text: "No", vec: [7, 3, -7, 1, 9, 1, -7, 8, 0, 2] },
-      { text: "Bicurious", vec: [2, 5, 0, 5, -9, 7, 4, 2, 1, 0]},
-      { text: "If I must (bicurious)", vec: [0, -8, -8, 8, 0, 6, 8, 1, 0, 5]},
-    ],
-  },
-  {
-    id: 8,
-    question: "Which data types are immutable?",
-    options: [
-      { text: "List", vec: [6, -3, 8, 1, -2, 2, 6, -5, 3, 7] },
-      { text: "Dictionary", vec: [-7, 5, 2, 8, 5, 7, 3, -4, -6, -4] },
-      { text: "Set", vec:  [1, 2, 1, 7, 0, 7, 9, -8, 0, 0] },
-      { text: "Tuple", vec: [3, 1, -9, 2, -1, 0, 7, 9, 0, 0] },
-    ],
-  },
-  {
-    id: 9,
-    question: "Would you say you are a organised or a happy person? ",
-    options: [
-      { text: "Very organised ...", vec: [5, -5, 8, 5, -2, 3, -2, 4, 1, 3] },
-      { text: "Organised ", vec: [5, 2, 6, -7, 1, 5, -7, -9, -5, 4]},
-      { text: "Happy and disorganised", vec: [8, 6, 4, -3, 5, -3, 1, 9, 3, 7] },
-      { text: "Very happy and very disorganised",vec: [9, 7, -2, -2, 2, 0, -4, 7, 8, 8]},
+      { text: "Curious", vec: [2, 5, 0, 5, -9, 7, 4, 2, 1, 0]},
+      { text: "If I must (still curious)", vec: [0, -8, -8, 8, 0, 6, 8, 1, 0, 5]},
     ],
   },
   {
@@ -105,6 +76,31 @@ export const questions: Array<Question> = [
       { text: "I have no experience with excel", vec: [2, 5, 0, 5, 9, 7, 4, 2, 1, 0] },
     ],
   },
+
+  {
+    id: 4,
+    question: "How close are you to retirement?",
+    options: [
+      { text: "This is my retirement party", vec: [-7, 2, 8, 7, -5, 5, 7, 6, -7, 4]},
+      { text: "Years....", vec: [0, -3, 9, 4, 5, -9, 0, 2, -9, 6] },
+      { text: "Hundreds of thousands", vec: [6, 2, 6, -7, 0, 6, -8, 0, 7, 9] },
+      { text: "I won't be able to retire", vec: [6, 6, 3, -0, 5, 0, 5, -4, -4, 3] },
+    ],
+  },
+
+  
+  {
+    id: 9,
+    question: "Would you say you are a organised or a happy person? ",
+    options: [
+      { text: "Very organised ...", vec: [5, -5, 8, 5, -2, 3, -2, 4, 1, 3] },
+      { text: "Organised ", vec: [5, 2, 6, -7, 1, 5, -7, -9, -5, 4]},
+      { text: "Happy and disorganised", vec: [8, 6, 4, -3, 5, -3, 1, 9, 3, 7] },
+      { text: "Very happy and very disorganised",vec: [9, 7, -2, -2, 2, 0, -4, 7, 8, 8]},
+    ],
+  },
+  
+  
   {
     id: 12,
     question: "Which conspiracy theory best represents your attitude in the workplace?",
@@ -133,6 +129,16 @@ export const questions: Array<Question> = [
       { text: "B", vec: [7, -2, 8, 7, -5, 5, -7, -6, 7, 4] },
       { text: "D", vec: [8, 3, -6, 1, -4, 0, 3, 9, -4, -8] },
       { text: "A", vec: [2, 5, -3, 7, 3, 1, -3, 1, 0, -2] },
+    ],
+  },
+  {
+    id: 8,
+    question: "Which data types are immutable?",
+    options: [
+      { text: "List", vec: [6, -3, 8, 1, -2, 2, 6, -5, 3, 7] },
+      { text: "Dictionary", vec: [-7, 5, 2, 8, 5, 7, 3, -4, -6, -4] },
+      { text: "Set", vec:  [1, 2, 1, 7, 0, 7, 9, -8, 0, 0] },
+      { text: "Tuple", vec: [3, 1, -9, 2, -1, 0, 7, 9, 0, 0] },
     ],
   },
   {
@@ -196,6 +202,16 @@ export const questions: Array<Question> = [
       { text: "Perl", vec: [-7, 5, 0, 0, 9, -5, 9, -8, 5, 0] },
       { text: "PHP", vec: [6, -7, 5, 8, -2, 1, 6, -3, 8, 1]},
       { text: "R", vec: [2, -8, 9, 8, -4, 9, 6, -2, 8, -5, 6] },
+    ],
+  },
+  {
+    id: 22,
+    question: "What comes next in the pattern <.//.. .. //?",
+    options: [
+      { text: "...", vec: [3, 0, 5, 7, -2, 7, 7, -3, 2, 1]},
+      { text: "./.", vec: [-1, 5, 1, 9, -9, 5, 2, -1, 3, -6] },
+      { text: "// .. ", vec: [-7, -3, 2, 1, 4, -8, 5, 5, -4, 2]},
+      { text: ".", vec: [1, 2, 3, -4, -5, 6, 7, -8, -9, 4, 1] },
     ],
   },
 ];
